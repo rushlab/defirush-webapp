e17311d6f3659705b0d2ae061572fdff180e3de5
web3nomad.eth <me@web3nomad.eth>
2021-07-30 23:57:01 +0800

    modify site meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,17 +5,17 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'DeFi Rush - The most trusted platform to manage digital assets',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'Track your full portfolio through various DeFi lending protocols on muliple chains'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
   },
   /*
    ** Customize the progress-bar color
