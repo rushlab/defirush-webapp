5b7ae7bf7d2946491a59c663ef932b1d468e64ed
web3nomad.eth <me@web3nomad.eth>
2021-08-13 23:43:16 +0800

    improve signer status checking

--- plugins/wallet/index.js
+++ plugins/wallet/index.js
@@ -5,112 +5,49 @@ import { MessageBox } from 'element-ui'
 import { WalletApp } from './wallet-app'
 
 
-async function verifyLoginData({ chainId, address, message, signature }) {
-  // TODO 还需要验证一下 chainID 对不对
-  const [_tip, _address, _timestamp] = message.split('\n')
-  if ((new Date()).valueOf() - _timestamp > 86400 * 7 * 1000) {
-    throw new Error('expired')
-  }
-  if (_address.toLowerCase() !== address.toLowerCase()) {
-    throw new Error('invalid address')
-  }
-  const signerAddress = await ethers.utils.verifyMessage(message, signature)
-  if (signerAddress.toLowerCase() !== address.toLowerCase()) {
-    throw new Error('invalid signature')
-  }
-  let connected = false
+async function checkSignerStatus(store) {
+  const { chainId, walletAddress } = store.state.auth
   let selectedChainId = 0
   let selectedAddress = ''
   if (typeof global.ethereum !== 'undefined' && global.ethereum.isMetaMask) {
     const provider = new ethers.providers.Web3Provider(global.ethereum)
     const signer = provider.getSigner()
     try {
-      selectedChainId = +((await signer.provider.getNetwork()).chainId)
-      selectedAddress = await signer.getAddress()
+      const [ network, address ] = await Promise.all([provider.getNetwork(), signer.getAddress()])
+      selectedChainId = +network.chainId
+      selectedAddress = address
     } catch(err) {}
   }
-  if (!selectedChainId || !selectedAddress) {
-    connected = false
-  } else if (+chainId === selectedChainId && address.toLowerCase() === selectedAddress.toLowerCase()) {
-    connected = true
+  if (chainId === selectedChainId && walletAddress.toLowerCase() === selectedAddress.toLowerCase()) {
+    store.commit('auth/setSignerStatus', true)
   } else {
-    throw new Error('address changed')
+    // 如果 metamask 里的地址和登录着的不同没关系, 依然可以看数据只是不能发送交易, 这种情况就是 isSignerAlive 为 false
+    store.commit('auth/setSignerStatus', false)
   }
-  // 如果无法读取地址, 但之前登录过, 这时候可以看自己的数据但是浏览器钱包不可用,
-  // 只有这种情况才会导致 isSignerAlive 是 false
-  return { connected }
-}
-
-
-function listenToMetaMask(store) {
-  const logoutAndReload = async () => {
-    if (store.state.auth.isAuthenticated) {
-      await store.dispatch('auth/logout')
-      MessageBox.confirm(
-        'Page will be reloaded because the network or your wallet account is changed',
-        'Network/Account Changed', {
-        confirmButtonText: 'OK',
-        cancelButtonText: 'DO NOT REFRESH',
-        type: 'warning'
-      }).then(() => global.location.reload()).catch(() => {})
-    }
-  }
-  const chainChanged = async () => {
-    const provider = new ethers.providers.Web3Provider(global.ethereum)
-    let selectedChainId = 0
-    try {
-      selectedChainId = +((await provider.getNetwork()).chainId)
-    } catch(error) {}  // 无法获取就直接忽略, 然后更新 signer 状态
-    if (!selectedChainId || store.state.auth.chainId !== selectedChainId) {
-      store.commit('auth/setSignerStatus', false)
-      await logoutAndReload()
-    }
-  }
-  const accountsChanged = async () => {
-    const { chainId, walletAddress } = store.state.auth
-    // if (!walletAddress) {
-    //   return
-    // }
-    const provider = new ethers.providers.Web3Provider(global.ethereum)
-    const signer = provider.getSigner()
-    let selectedChainId = 0
-    let selectedAddress = ''
-    try {
-      selectedChainId = +((await signer.provider.getNetwork()).chainId)
-      selectedAddress = await signer.getAddress()
-    } catch(error) {}  // 无法获取就直接忽略, 然后更新 signer 状态
-    if (!selectedChainId || !selectedAddress) {
-      store.commit('auth/setSignerStatus', false)
-    } else if (+chainId === selectedChainId && walletAddress.toLowerCase() === selectedAddress.toLowerCase()) {
-      store.commit('auth/setSignerStatus', true)
-    } else {
-      await logoutAndReload()
-    }
-  }
-  if (typeof global.ethereum !== 'undefined' && global.ethereum.isMetaMask) {
-    global.ethereum.on('chainChanged', chainChanged)
-    global.ethereum.on('accountsChanged', accountsChanged)
-  }
+  // MessageBox.confirm(
+  //   'Page will be reloaded because the network or your wallet account is changed',
+  //   'Network/Account Changed', {
+  //   confirmButtonText: 'OK',
+  //   cancelButtonText: 'DO NOT REFRESH',
+  //   type: 'warning'
+  // }).then(() => global.location.reload()).catch(() => {})
 }
 
 
 export default async ({ store }) => {
+  /**
+   * 只处理 signer status, 这里不需要处理登录状态, store 里都处理好了
+   */
+  await checkSignerStatus(store)
 
-  const loginData = await store.dispatch('auth/getLoginData')
-  if (loginData) {
-    try {
-      const { connected } = await verifyLoginData(loginData)
-      const { address } = loginData
-      store.commit('auth/setWallet', address)
-      store.commit('auth/setSignerStatus', connected)
-    } catch(error) {
-      console.log(error)
-      await store.dispatch('auth/logout')
-    }
+  /**
+   * 现在只支持 metamask, 回头再添加其他的
+   */
+  if (typeof global.ethereum !== 'undefined' && global.ethereum.isMetaMask) {
+    const handler = () => checkSignerStatus(store)
+    global.ethereum.on('chainChanged', handler)
+    global.ethereum.on('accountsChanged', handler)
   }
-
-  // 现在只支持 metamask, 回头再添加其他的
-  listenToMetaMask(store)
 
   /**
    * 需要 auth store 初始化好了再 getTokens, 但是要再 $wallet 初始化之前
--- store/auth.js
+++ store/auth.js
@@ -3,6 +3,43 @@ import { chains as ALL_CHAINS_LIST } from '@/utils/chains'
 const CHAIN_STORAGE_KEY = 'web3-chain-id'
 const AUTH_STORAGE_KEY = 'web3-wallet-auth'
 
+const getStateFromStorage = () => {
+  const state = {
+    chainId: 1,
+    walletAddress: '',
+    _apiToken: '',
+  }
+  /* determine chainId */
+  const chainId = +global.localStorage.getItem(CHAIN_STORAGE_KEY)
+  if (_.find(ALL_CHAINS_LIST, { chainId })) {
+    state.chainId = chainId
+  }
+  /* decode login data */
+  try {
+    const content = global.localStorage.getItem(AUTH_STORAGE_KEY)
+    const { chainId, address, message, signature } = JSON.parse(content)
+    const [_tip, _address, _timestamp] = message.split('\n')
+    if ((new Date()).valueOf() - (+_timestamp) > 86400 * 7 * 1000) {
+      throw new Error('expired')
+    }
+    if (_address.toLowerCase() !== address.toLowerCase()) {
+      throw new Error('invalid address')
+    }
+    const signerAddress = ethers.utils.verifyMessage(message, signature)
+    if (signerAddress.toLowerCase() !== address.toLowerCase()) {
+      throw new Error('invalid signature')
+    }
+    // 执行到这里 content 就没问题, 然后再 _setApiToken
+    state._apiToken = btoa(content)
+    state.walletAddress = address
+  } catch(error) {
+    console.error(error)
+    global.localStorage.removeItem(AUTH_STORAGE_KEY)
+  }
+  /**/
+  return state
+}
+
 /**
  * chainId 和 address 可以唯一确定一个用户, 不需要在 store 里面记录使用了什么钱包接口
  * 使用什么钱包接口在获取 signer 的时候判断, signer 软件的切换不影响用户本身的信息
@@ -12,16 +49,13 @@ const AUTH_STORAGE_KEY = 'web3-wallet-auth'
  * isSignerAlive: 浏览器钱包可用, 可以发送交易, 并且和 walletAddress 对应
  */
 export const state = () => {
-  let chainId = +global.localStorage.getItem(CHAIN_STORAGE_KEY)
-  if (!_.find(ALL_CHAINS_LIST, { chainId })) {
-    chainId = 1
-  }
+  const { chainId, walletAddress, _apiToken } = getStateFromStorage()
   return {
     chainId,  // chainId 一定存在于 ALL_CHAINS_LIST, 其他地方可以放心使用
-    walletAddress: '',
-    isAuthenticated: false,
+    walletAddress,
+    _apiToken,  // 只给 axios 用
+    isAuthenticated: !!walletAddress,
     isSignerAlive: false,
-    _apiToken: '',  // 只给 axios 用
   }
 }
 
