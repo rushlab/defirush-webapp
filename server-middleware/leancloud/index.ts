e1f1e255e077883b4d659fa2cffd231be5ed487e
ranlix <wrapped.akira@gmail.com>
2021-09-24 18:21:37 +0800

    add default LEANCLOUD_APPKEY

--- server-middleware/leancloud/index.ts
+++ server-middleware/leancloud/index.ts
@@ -2,7 +2,7 @@ import AV from 'leancloud-storage'
 
 // -MdYXbMMI 结尾的就不是 CN region, 不需要提供 serverURL
 const LEANCLOUD_APPID = process.env.LEANCLOUD_APPID || '000-MdYXbMMI'
-const LEANCLOUD_APPKEY = process.env.LEANCLOUD_APPKEY || ''
+const LEANCLOUD_APPKEY = process.env.LEANCLOUD_APPKEY || '000'
 AV.init({ appId: LEANCLOUD_APPID, appKey: LEANCLOUD_APPKEY })
 
 export const LeanCloudStorage = AV
